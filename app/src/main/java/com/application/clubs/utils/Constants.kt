639b75b0ac3ac85a94c4d3ff0da375a56a2df891
VLAD\micro <BenedettiV661@proton.me>
2024-05-31 19:13:14 +0300

    Коммит

--- app/src/main/java/com/application/clubs/utils/Constants.kt
+++ app/src/main/java/com/application/clubs/utils/Constants.kt
@@ -3,4 +3,4 @@ package com.application.clubs.utils
 const val BASE_URL = "http://teammanager.dealko.org/api/"
 const val TEAM_ID = 1
 const val URL = "http://teammanager.dealko.org"
-const val TOKEN = ""
+
